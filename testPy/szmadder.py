23427ba5957c661c5c01044aaece08ddc4cebce8
lws <liuweishuo@126.com>
2023-02-21 16:08:32 +0800

    remove testPy

--- testPy/szmadder.py
+++
@@ -1,8 +0,0 @@
-#! env python
-class Adder(object):
-    def __init__(self):
-        print('calling constructor')
-        self.info = 'default info'
-    def add(self, a, b):
-        print(f'Adder info is {self.info}')
-        return a + b
